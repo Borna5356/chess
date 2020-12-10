5715164b3b1ff38487bee3289074175c7efa92f8
Borna5356 <bornakevin1@gmail.com>
2020-12-10 18:23:47 -0500

    refactored the move_pawn function

--- moves.py
+++ moves.py
@@ -13,72 +13,17 @@ def move_pawn(pawn, board):
     a pawn can do
 
     """
-    current_position = pawn.get_position()
-    chess_board = board.get_board()
-    color = pawn.get_color()
-    new_positions = []
+
     diagonal_black_moves = [(1, 1), (1, -1)]
     vertical_black_moves = [(1, 0)]
     diagonal_white_moves = [(-1, 1), (-1, -1)]
     vertical_white_moves = [(-1, 0)]
-    has_moved = pawn.get_has_moved()
 
     if (pawn.get_color() == "black"):
-        if (has_moved == False):
-            vertical_black_moves.append((2, 0))
-            pawn.set_has_moved()
-
-        for move in vertical_black_moves:
-            new_row = current_position[0] + move[0]
-            new_col = current_position[1] + move[1]
-            if (new_row < 0) or (new_row > 7) or (new_col < 0) or (new_col > 7):
-                pass
-            else:
-                if (chess_board[new_row][new_col] == '-'):
-                    new_positions.append((new_row, new_col))
-                else:
-                    pass
-
-        for move in diagonal_black_moves:
-            new_row = current_position[0] + move[0]
-            new_col = current_position[1] + move[1]
-            if (new_row < 0) or (new_row > 7) or (new_col < 0) or (new_col > 7):
-                continue
-            else:
-                if (chess_board[new_row][new_col] != '-'):
-                    other_piece = chess_board[new_row][new_col]
-                    if (pawn.get_color() != other_piece.get_color()):
-                        new_positions.append((new_row, new_col))
-                    else:
-                        pass
+        new_positions = find_positions(pawn, board, vertical_black_moves, diagonal_black_moves)
 
     else:
-        if (has_moved == False):
-            vertical_white_moves.append((-2, 0))
-            pawn.set_has_moved()
-        for move in vertical_white_moves:
-            new_row = current_position[0] + move[0]
-            new_col = current_position[1] + move[1]
-            if (new_row < 0) or (new_row > 7) or (new_col < 0) or (new_col > 7):
-                pass
-            else:
-                if (chess_board[new_row][new_col] == '-'):
-                    new_positions.append((new_row, new_col))
-                else:
-                    pass
-
-        for move in diagonal_white_moves:
-            new_row = current_position[0] + move[0]
-            new_col = current_position[1] + move[1]
-            if (new_row < 0) or (new_row > 7) or (new_col < 0) or (new_col > 7):
-                continue
-            else:
-                if (chess_board[new_row][new_col] != '-'):
-                    other_piece = chess_board[new_row][new_col]
-                    if (pawn.get_color() != other_piece.get_color()):
-                        new_positions.append((new_row, new_col))
-                    else:
-                        pass
+        new_positions = find_positions(pawn, board, vertical_white_moves, diagonal_white_moves)
 
     new_position = input("Enter the position to move your pawn to Ex.(2 3): ")
     new_position = new_position.split(' ')
@@ -92,6 +37,50 @@ def move_pawn(pawn, board):
         new_col = int(new_position[0])
 
     return (new_row, new_col)
+
+def find_positions(pawn, board, vertical_moves, diagonal_moves):
+
+    """
+    decides the moves available
+    for the black pawn
+
+    """
+    new_positions = []
+    current_position = pawn.get_position()
+    chess_board = board.get_board()
+    has_moved = pawn.get_has_moved()
+
+    if (has_moved == False):
+        if (pawn.get_color() == "black"):
+            vertical_moves.append((2, 0))
+        else:
+            vertical_moves.append((-2, 0))
+        pawn.set_has_moved()
+
+    for move in vertical_moves:
+        new_row = current_position[0] + move[0]
+        new_col = current_position[1] + move[1]
+        if (new_row < 0) or (new_row > 7) or (new_col < 0) or (new_col > 7):
+            pass
+        else:
+            if (chess_board[new_row][new_col] == '-'):
+                new_positions.append((new_row, new_col))
+            else:
+                pass
+
+    for move in diagonal_moves:
+        new_row = current_position[0] + move[0]
+        new_col = current_position[1] + move[1]
+        if (new_row < 0) or (new_row > 7) or (new_col < 0) or (new_col > 7):
+            continue
+        else:
+            if (chess_board[new_row][new_col] != '-'):
+                other_piece = chess_board[new_row][new_col]
+                if (pawn.get_color() != other_piece.get_color()):
+                    new_positions.append((new_row, new_col))
+                else:
+                    pass
+    return new_positions
 
 def move_knight(knight, board):
     """
