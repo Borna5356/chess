095d087e8f0ef366e9d0f30abaf0162fba7e1afd
Borna5356 <bornakevin1@gmail.com>
2021-02-02 11:45:37 -0500

    added error handling for choosing piece

--- player.py
+++ player.py
@@ -9,9 +9,13 @@ def choose_piece(chess_board, color):
         if (position == "stop"):
                 return False
         else:
-                position = position.split(' ')
-                row = int(position[1])
-                column = int(position[0])
+                try:
+                        position = position.split(' ')
+                        row = int(position[1])
+                        column = int(position[0])
+                except IndexError:
+                        print("The coordianates were entered incorrectly")
+                        return None
                 if (0 <= row <= 7) and (0 <= column <= 7):
                         piece = chess_board.get_board()[row][column]
                         if (piece == '-'):
